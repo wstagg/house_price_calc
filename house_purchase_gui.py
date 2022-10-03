dd527d0f98e9d2244c9f01ee7034412b2858550e
Wesley <wes.stagg@gmail.com>
2022-10-03 23:28:32 +0100

    MInor edits

--- house_purchase_gui.py
+++ house_purchase_gui.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import ttk
-from unittest.loader import VALID_MODULE_NAME
+
 
 def calculate_house_budget(*args):
     try:
@@ -9,7 +9,7 @@ def calculate_house_budget(*args):
         equit = int(equity.get())
         fee = int(fees.get())
         mort = int(mortgage.get())
-        house_budget.set((save_1 + save_2 + equit + mort)-fee) 
+        house_budget.set(f'£{"{:,}".format((save_1 + save_2 + equit + mort)-fee)}') 
     except ValueError:
         pass
 
@@ -18,7 +18,7 @@ def calulate_total_deposit(*args):
         save_1 = int(savings_1.get())
         save_2 = int(savings_2.get())
         equit = int(equity.get())
-        deposit_total.set(save_1 + save_2 + equit)
+        deposit_total.set(f'£{"{:,}".format(save_1 + save_2 + equit)}')
     except ValueError:
         pass
 
@@ -26,9 +26,9 @@ def calculate_amount_to_save(*args):
     try:
         current_house_budget = ((int(savings_1.get()) + int(savings_2.get()) + int(equity.get()) + int(mortgage.get())) - int(fees.get()))
         if int(goal.get()) > current_house_budget:
-            total_to_save.set(int(goal.get()) - current_house_budget)
+            total_to_save.set(f'£{"{:,}".format(int(goal.get()) - current_house_budget)}')
         else:
-            total_to_save.set(0)
+            total_to_save.set("£0")
     except ValueError:
         pass
 
@@ -66,19 +66,20 @@ goal_entry = ttk.Entry(mainframe, width=7, textvariable=goal)
 goal_entry.grid(column=2, row=6, sticky=(E, W))
 
 # Create Buttons
-ttk.Button(mainframe, text="Calculate house budget", command=calculate_house_budget).grid(column=1, row=7, sticky=W)
-ttk.Button(mainframe, text="Calculate deposit", command=calulate_total_deposit).grid(column=1, row=8, sticky=W)
-ttk.Button(mainframe, text="Calculate amount left to save", command=calculate_amount_to_save).grid(column=1, row=9, sticky=W)
+ttk.Button(mainframe,text="Show house budget", command=calculate_house_budget).grid(column=1, row=7, sticky=W)
+ttk.Button(mainframe, text="Show deposit", command=calulate_total_deposit).grid(column=1, row=8, sticky=W)
+ttk.Button(mainframe, text="Show amount to save", command=calculate_amount_to_save).grid(column=1, row=9, sticky=W)
+
 
 # Create output labels 
 house_budget = StringVar()
-ttk.Label(mainframe, textvariable=house_budget).grid(column=2, row=7, sticky=(W, E))
+ttk.Label(mainframe, relief='sunken', textvariable=house_budget).grid(column=2, row=7, sticky=(W, E))
 
 deposit_total = StringVar()
-ttk.Label(mainframe, textvariable=deposit_total).grid(column=2, row=8, sticky=(W, E))
+ttk.Label(mainframe,relief='sunken', textvariable=deposit_total).grid(column=2, row=8, sticky=(W, E))
 
 total_to_save = StringVar()
-ttk.Label(mainframe, textvariable=total_to_save).grid(column=2, row=9, sticky=(W, E))
+ttk.Label(mainframe,relief='sunken', textvariable=total_to_save).grid(column=2, row=9, sticky=(W, E), )
 
 # Create text labels 
 ttk.Label(mainframe, text="Savings #1").grid(column=1, row=1, sticky=W)
@@ -89,6 +90,6 @@ ttk.Label(mainframe, text="Mortgage").grid(column=1, row=5, sticky=W)
 ttk.Label(mainframe, text="Goal").grid(column=1, row=6, sticky=W)
 
 for child in mainframe.winfo_children(): 
-    child.grid_configure(padx=5, pady=5)
+    child.grid_configure(padx=6, pady=6)
 
 root.mainloop()
